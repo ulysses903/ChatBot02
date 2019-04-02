15b1e2966f26c3fbfaaca58737bab919d4ab4f68
ulysses903 <ulysses903@gmail.com>
2019-04-02 11:28:42 -0300

    first commit

---
+++ ChatBotTeste01-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotteste01-Web-Deploy.pubxml -p:Password=ir9rxGaYwjh7o3vovsPj03xiYr78fsLvy0wniQjxl2lexpwlZNvGH1ludaCK
+
