85676f5d69b917987d18983b4ff6462e44e5dd1e
ulysses903 <ulysses903@gmail.com>
2019-04-02 11:42:13 -0300

    commit correcao

--- ChatBotTeste01-src/Bots/EchoBot.cs
+++ ChatBotTeste01-src/Bots/EchoBot.cs
@@ -11,46 +11,20 @@ namespace Microsoft.Bot.Builder.EchoBot
 {
     public class EchoBot : ActivityHandler
     {
-        public EchoBot()
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            await turnContext.SendActivityAsync(MessageFactory.Text($"Teste de Eco 123: {turnContext.Activity.Text}"), cancellationToken);
         }
 
-        /// <summary>
-        /// Every conversation turn calls this method.
-        /// </summary>
-        /// <param name="turnContext">A <see cref="ITurnContext"/> containing all the data needed
-        /// for processing this conversation turn. </param>
-        /// <param name="cancellationToken">(Optional) A <see cref="CancellationToken"/> that can be used by other objects
-        /// or threads to receive notice of cancellation.</param>
-        /// <returns>A <see cref="Task"/> that represents the work queued to execute.</returns>
-        /// <seealso cref="BotStateSet"/>
-        /// <seealso cref="ConversationState"/>
-        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            // Handle Message activity type, which is the main activity type for shown within a conversational interface
-            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
-            // see https://aka.ms/about-bot-activity-message to learn more about the message and other activity types
-            string responseMessage = turnContext.Activity.Text;
-
-            if (responseMessage.ToLower() == "ola" || responseMessage.ToLower() == "oi")
+            foreach (var member in membersAdded)
             {
-                await turnContext.SendActivityAsync("Ola, qual seu nome?");
-            }
-
-            else if (responseMessage == "Ulysses")
-            {
-                await turnContext.SendActivityAsync("Muito prazer Ulysses. O que Gostaria?");
-            }
-
-            else if (responseMessage == "Um estagio")
-            {
-                await turnContext.SendActivityAsync("Peça ao Mauricio");
-            }
-
-            else
-            {
-                // Echo back to the user whatever they typed.             
-                await turnContext.SendActivityAsync("Tente outra pergunta.", cancellationToken: cancellationToken);
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to Echo Bot."), cancellationToken);
+                }
             }
         }
+    }
 }
